775bd3fc8bfc6f1b940ac70d689f5e0b0e88b1cd
sevak-martirosyan <sevak707@gmail.com>
2018-02-12 09:42:44 +0400

    commit

--- app/src/main/java/com/orion/authapp/Application.java
+++ app/src/main/java/com/orion/authapp/Application.java
@@ -2,15 +2,9 @@ package com.orion.authapp;
 
 import android.content.Intent;
 
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKAccessTokenTracker;
 import com.vk.sdk.VKSdk;
-
-/**
- * Created by orion on 2/11/18.
- */
 
 public class Application extends android.app.Application {
 
@@ -18,7 +12,7 @@ public class Application extends android.app.Application {
         @Override
         public void onVKAccessTokenChanged(VKAccessToken oldToken, VKAccessToken newToken) {
             if (newToken == null) {
-                Intent intent = new Intent(Application.this,MainActivity.class);
+                Intent intent = new Intent(Application.this, MainActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
             }
@@ -33,7 +27,5 @@ public class Application extends android.app.Application {
         vkAccessTokenTracker.startTracking();
         VKSdk.initialize(this);
 
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
     }
 }
--- app/src/main/java/com/orion/authapp/MainActivity.java
+++ app/src/main/java/com/orion/authapp/MainActivity.java
@@ -1,18 +1,41 @@
 package com.orion.authapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.FacebookSdk;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
 import com.vk.sdk.api.VKError;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    SignInButton signInButton;
+    Button signOutButton;
+    TextView statusTextView;
+    GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
 
 
     private String[] scope = new String[]{VKScope.MESSAGES, VKScope.FRIENDS, VKScope.WALL};
@@ -23,40 +46,106 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        statusTextView = findViewById(R.id.status_text_view);
+
+        signInButton = findViewById(R.id.gl_sign_in_button);
+        signInButton.setOnClickListener(this);
+
+        signOutButton = findViewById(R.id.gl_sign_out_button);
+        signOutButton.setOnClickListener(this);
+
+
     }
 
-    public void onClickGoogle(View view){
-
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.gl_sign_in_button:
+                signIn();
+                break;
+            case R.id.gl_sign_out_button:
+                signOut();
+                break;
+        }
     }
 
-    public void onClickFacebook(View view){
-
-    }
-
-    public void onClickVk(View view){
-
-        VKSdk.login(this,scope);
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
 
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+            Toast.makeText(getApplicationContext(), "Good job!", Toast.LENGTH_LONG).show();
+        }
+
         if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
             @Override
             public void onResult(VKAccessToken res) {
-
-                Toast.makeText(getApplicationContext(),"Good job!",Toast.LENGTH_LONG).show();
+                statusTextView.setText(res.userId + " is connected!");
+                Toast.makeText(getApplicationContext(), "Good job!", Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onError(VKError error) {
 
-                Toast.makeText(getApplicationContext(),"Error!",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Error!", Toast.LENGTH_LONG).show();
             }
 
         })) {
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, " handleSignInResult:" + result.isSuccess());
+
+        if (result.isSuccess()) {
+            GoogleSignInAccount acct = result.getSignInAccount();
+            statusTextView.setText(acct.getDisplayName() + " is connected!");
+        } else {
+            Toast.makeText(getApplicationContext(), "Error!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFiled" + connectionResult);
+
+
+    }
+
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                statusTextView.setText("Signed out");
+            }
+        });
+    }
+
+    public void onClickFacebook(View view) {
+
+
+    }
+
+    public void onClickVk(View view) {
+
+        VKSdk.login(this, scope);
+
     }
 
 
