715644fb4240e35dcc18cf72100ffe396a8abff6
dell <dell@linux.Dlink>
2020-06-20 11:39:49 +0400

    esh commit

--- app/src/main/java/com/orion/authapp/Application.java
+++ app/src/main/java/com/orion/authapp/Application.java
@@ -7,6 +7,8 @@ import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKAccessTokenTracker;
 import com.vk.sdk.VKSdk;
 
+import com.vk.sdk.VKSdk;
+
 public class Application extends android.app.Application {
 
     VKAccessTokenTracker vkAccessTokenTracker = new VKAccessTokenTracker() {
